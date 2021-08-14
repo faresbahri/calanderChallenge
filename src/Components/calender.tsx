df5eab7f9d5843ff7d0e1e66ca8b584be51c3d91
faresbahri <bahrifaires@gmail.com>
2021-08-14 03:29:04 +0100

    Temp passé (2jours): 12H de travail

---
+++ src/Components/calender.tsx
@@ -0,0 +1,12 @@
+import { CalendarComponent } from '@syncfusion/ej2-react-calendars';
+import * as React from "react";
+import '../App.css';
+
+export class Calender extends React.Component<{}, {}> {
+public render() {
+    return <CalendarComponent id="calendar" />
+}
+
+};
+
+
---
+++ src/Containers/App.tsx
@@ -0,0 +1,17 @@
+import '../App.css';
+import { Calender } from '../Components/calender';
+import { Schedule } from '../Components/schedule';
+
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Calender />
+      <br></br>
+      <Schedule />
+    </div>
+  );
+}
+
+export default App;
